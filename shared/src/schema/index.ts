77f2bee058e5e2ce33eb306679b97b8cc7870f32
Czvein Lei Unarce <czveinlei17@gmail.com>
2026-02-15 01:10:36 +0800

    Refactor schema definitions to include taskLogSchema and improve type inference for task and task log creation

--- shared/src/schema/index.ts
+++ shared/src/schema/index.ts
@@ -1,6 +1,15 @@
-import { taskSchema } from "server/dist/db/schema";
-import type z from "zod";
-
+import { taskLogSchema, taskSchema } from "server/dist/db/schema";
+import { z as zod } from "zod";
+import zType from "zod";
+// TASK
 export const createTaskSchema = taskSchema.omit({ id: true, runId: true });
 
-export type CreateTask = z.infer<typeof createTaskSchema>;
+export const createTaskMultipleSchema = zod.array(createTaskSchema);
+
+export type CreateTask = zType.infer<typeof createTaskSchema>;
+
+// TAKS LOGS
+
+export const createTaskLogsSchema = taskLogSchema.omit({ id: true });
+
+export type CreateTaskLog = zType.infer<typeof createTaskLogsSchema>;
