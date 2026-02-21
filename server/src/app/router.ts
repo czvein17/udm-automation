532bace31660d057fba1d1227c6addda13e6ac52
Czvein Lei Unarce <czveinlei17@gmail.com>
2026-02-21 16:43:53 +0800

    refactor(runs): remove unused runs routes and handler

--- server/src/app/router.ts
+++ server/src/app/router.ts
@@ -1,6 +1,5 @@
 import { Hono } from "hono";
 
-import { runsRoutes } from "@server/feature/runs/runs.route";
 import { automationRoute } from "@server/feature/automation/automation.route";
 import { taskRoutes } from "@server/feature/task/task.route";
 import { configRoute } from "@server/feature/config/config.route";
@@ -10,6 +9,5 @@ export const appRouter = new Hono()
   .get("/", (c) => c.json({ message: "Hello from the main app router!" }))
   .route("/automation", automationRoute)
   .route("/reporter", reporterRoutes)
-  .route("/runs", runsRoutes)
   .route("/tasks", taskRoutes)
   .route("/configs", configRoute);
--- server/src/feature/runs/runs.route.ts
+++
@@ -1,6 +0,0 @@
-import { Hono } from "hono";
-import { getAllRunByRunId, getRunById } from "./runs.handler";
-
-export const runsRoutes = new Hono()
-  .get("/", getAllRunByRunId)
-  .get("/:runId", getRunById);
